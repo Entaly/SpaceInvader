adfea071fa5ec75d6f2b798b87ffb8cc70d52920
Aurelien <aurelien.ravaud@etu.unilim.fr>
2019-04-29 10:53:26 +0200

    positionner un nouveau vaisseau

---
+++ src/main/java/fr/unilim/iut/spaceinvaders/utils/HorsEspaceJeuException.java
@@ -0,0 +1,9 @@
+package fr.unilim.iut.spaceinvaders.utils;
+
+
+public class HorsEspaceJeuException extends RuntimeException {
+	public HorsEspaceJeuException(String message) {
+	    super(message);
+    }
+	
+}
