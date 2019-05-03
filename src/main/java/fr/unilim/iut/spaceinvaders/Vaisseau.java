a37bee6311172c846c2df336c90477b31293e5d1
Aurelien <aurelien.ravaud@etu.unilim.fr>
2019-05-03 08:43:06 +0200

    refactoring classe Vaisseau TP3

--- src/main/java/fr/unilim/iut/spaceinvaders/Vaisseau.java
+++ src/main/java/fr/unilim/iut/spaceinvaders/Vaisseau.java
@@ -4,24 +4,69 @@ public class Vaisseau {
 
 	int x;
 	int y;
+	int longueur;
+	int hauteur;
 
-	public Vaisseau(int x, int y) {
-		this.x = x;
-		this.y = y;
+	public Vaisseau(int longueur, int hauteur) {
+		this(longueur, hauteur, 0, 0);
 	}
-    public boolean occupeLaPosition(int x, int y) {
-		return (this.x==x) && (this.y==y);
+	
+	 public Vaisseau(int longueur, int hauteur, int x, int y) {
+		   this.longueur=longueur;
+		   this.hauteur=hauteur;
+		   this.x = x;
+		   this.y = y;
+	    }
+
+	 public boolean occupeLaPosition(int x, int y) {
+	     if (estAbscisseCouverte(x) && estOrdonneeCouverte(y))
+			return true;
+		
+	     return false;
+     }
+
+	private boolean estOrdonneeCouverte(int y) {
+		return ordonneLaPlusBasse(y) && ordonneeLaPlusHaute(y);
 	}
+
+	private boolean ordonneeLaPlusHaute(int y) {
+		return y<=this.y;
+	}
+
+	private boolean ordonneLaPlusBasse(int y) {
+		return this.y-this.hauteur+1<=y;
+	}
+
+	private boolean estAbscisseCouverte(int x) {
+		return (abscisseLaPlusAGauche()<=x) && (x<=abscisseLaPlusADroite());
+	}
+
+	public int abscisseLaPlusAGauche() {
+		return this.x;
+	}
+
+	public int abscisseLaPlusADroite() {
+		return abscisseLaPlusAGauche()+this.longueur-1;
+	}
+	 
+	 
     public void seDeplacerVersLaDroite() {
-	      this.x = this.x + 1 ;
+	      this.x = abscisseLaPlusAGauche() + 1 ;
  }
 	public int abscisse() {
-        return this.x;
+        return abscisseLaPlusAGauche();
 	}
     public void seDeplacerVersLaGauche() {
-	      this.x = this.x - 1 ;
+	      this.x = abscisseLaPlusAGauche() - 1 ;
 	      
 }
+
+	public void positionner(int x, int y) {
+		this.x = x;
+	    this.y = y;
+		
+	}
+  
 
 
 }
